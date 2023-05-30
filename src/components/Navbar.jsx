8c01beb4cf834e9b2682eced2ace35481f17be8e
Omar Hamid <omarhamid664@gmail.com>
2023-05-29 18:19:52 -0600

    Complete navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,17 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBarsStaggered } from "@fortawesome/free-solid-svg-icons";
+
+import "styles/Navbar.scss";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <p className="navbar-hero-name">Omar Hamid</p>
+      <div>
+        <FontAwesomeIcon icon={faBarsStaggered} />
+      </div>
+    </div>
+  );
 }
 
 export default Navbar;
