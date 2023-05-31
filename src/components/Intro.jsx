bd6fae28a18b727b76b3ba2be74d09b77ffcd1f4
Omar Hamid <omarhamid664@gmail.com>
2023-05-30 22:56:25 -0600

    Responsive Intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,53 +6,56 @@ import "styles/Intro.scss";
 
 function About() {
   return (
-    <div className="intro-container" id='home'>
+    <div className="intro-container" id="home">
       <div className="about">
-        <div
-          className="image"
-          style={{ backgroundImage: "url(/my-photo.jpg)" }}
-        />
-        <div className="about-text">
-          <div className="about-title">
-            <h1>Full-Stack Developer</h1>
-            <img src="/hand.png" alt="waving-hand" />
+        <div className="intro-text-img-container">
+          <div
+            className="image"
+            style={{ backgroundImage: "url(/my-photo.jpg)" }}
+          />
+          <div className="about-text">
+            <div className="about-title">
+              <h1>Full-Stack Developer</h1>
+              <img src="/hand.png" alt="waving-hand" />
+            </div>
+            <p>
+              Hi, I'm Omar. A passionate Full-Stack Web Developer based in
+              Calgary, Alberta 📍
+            </p>
+            <div className="socials">
+              <a
+                href="https://www.linkedin.com/in/omarhamid664/"
+                target="_blank"
+                rel="noreferrer"
+                id="linkedIn-link"
+              >
+                <FontAwesomeIcon icon={faLinkedin} />
+              </a>
+              <a
+                href="https://github.com/Heaven664"
+                target="_blank"
+                rel="noreferrer"
+                id="github-link"
+              >
+                <FontAwesomeIcon icon={faGithubSquare} />
+              </a>
+            </div>
           </div>
-          <p>
-            Hi, I'm Omar. A passionate Full-Stack Web Developer based in
-            Calgary, Alberta 📍
-          </p>
-          <div className="socials">
-            <a
-              href="https://www.linkedin.com/in/omarhamid664/"
-              target="_blank"
-              rel="noreferrer"
-              id="linkedIn-link"
-            >
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <a
-              href="https://github.com/Heaven664"
-              target="_blank"
-              rel="noreferrer"
-              id="github-link"
-            >
-              <FontAwesomeIcon icon={faGithubSquare} />
-            </a>
-          </div>
-          <div className="tech-stack">
-            <p>Tech Stack</p>
-            <ul>
-              {skillsIcons.map((icon) => (
-                <li key={icon.title}>
-                  <img
-                    src={`/skills/${icon.img}.png`}
-                    title={icon.title}
-                    alt={icon.img}
-                  />
-                </li>
-              ))}
-            </ul>
-          </div>
+        </div>
+
+        <div className="tech-stack">
+          <p>Tech Stack</p>
+          <ul>
+            {skillsIcons.map((icon) => (
+              <li key={icon.title}>
+                <img
+                  src={`/skills/${icon.img}.png`}
+                  title={icon.title}
+                  alt={icon.img}
+                />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
