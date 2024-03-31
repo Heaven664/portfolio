3e2d49169a9633219fe112b73b092a95ae55b014
Omar Hamid <omarhamid664@gmail.com>
2024-03-31 10:28:18 -0600

    Add messenger project

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faIdCard } from "@fortawesome/free-solid-svg-icons";
 import skillsIcons from "helpers/skills";
 
 import "styles/Intro.scss";
@@ -23,6 +24,14 @@ function About() {
               Calgary, Alberta 📍
             </p>
             <div className="socials">
+              <a
+                href="https://flowcv.com/resume/2nf3jktqjs"
+                target="_blank"
+                rel="noreferrer"
+                id="resume"
+              >
+                <FontAwesomeIcon icon={faIdCard} />
+              </a>
               <a
                 href="https://www.linkedin.com/in/omarhamid664/"
                 target="_blank"
